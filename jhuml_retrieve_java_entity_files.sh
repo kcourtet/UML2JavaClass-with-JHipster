85fb155cf7afb6bb89cf1160659725e4806a96aa
kcourtet <36259702+kcourtet@users.noreply.github.com>
2018-03-21 16:14:57 +0100

    java file generation with input args

--- jhuml_retrieve_java_entity_files.sh
+++ jhuml_retrieve_java_entity_files.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Output folder
-mkdir res/jhuml_generated_classes/
 # Input file parameter given by the user
 FILE=$1
+OUT=$2
 
 # JHipster-uml launch with the input file
-echo Start && jhipster-uml $FILE && \
-cp src/main/java/com/mycompany/myapp/domain/* res/jhuml_generated_classes/ && \
-cd res/jhuml_generated_classes/ && ls && echo End
+echo Start && jhipster-uml $FILE 
+
+mkdir output
+if [ -z $OUT ]
+  then 
+      cp src/main/java/com/mycompany/myapp/domain/* output/ 
+      cd output/ && ls
+  else
+      mkdir $OUT
+      cp src/main/java/com/mycompany/myapp/domain/* $OUT/
+      rm -r output/
+      cd $OUT/ && ls
+fi
+
+echo End
